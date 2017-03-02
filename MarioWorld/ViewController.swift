028b85923624bd5e53839b73dfe5e03d78444a9e
Oleg Shulakov <shulakov_oleg@mail.ru>
2017-03-02 20:04:59 +0400

    Initial Commit

---
+++ MarioWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarioWorld
+//
+//  Created by Oleg Shulakov on 02.03.17.
+//  Copyright © 2017 Oleg Shulakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
